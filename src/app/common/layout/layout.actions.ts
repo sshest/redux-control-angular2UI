493fdd1429133498ee2621340074896ef8f8f78a
sergey.shestakov <sergey.shestakov@uadevelopers.com>
2017-09-19 17:15:22 +0300

    Layout Modal actions added

--- src/app/common/layout/layout.actions.ts
+++ src/app/common/layout/layout.actions.ts
@@ -4,7 +4,23 @@ import {Action} from '@ngrx/store';
 
 export const LayoutActionTypes = {
 
+    //modal action types
+    OPEN_MODAL: '[layout] Open modal',
+    CLOSE_MODAL: '[layout] close modal'
 };
 
+export class OpenModalAction {
+    type = LayoutActionTypes.OPEN_MODAL
+    constructor(public payload:string) {
 
-export type LayoutActions = null;
+    }
+}
+
+export class CloseModalAction {
+    type = LayoutActionTypes.CLOSE_MODAL
+    constructor(public payload:string) {
+
+    }
+}
+
+export type LayoutActions = OpenModalAction | CloseModalAction;
