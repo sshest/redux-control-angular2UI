e6d39243240f94bed4fb651639806ae0ca7142d7
sergey.shestakov <sergey.shestakov@uadevelopers.com>
2017-09-19 15:55:38 +0300

    metaReducer added (it returns wrapped with logger combined reducer for use in development)

---
+++ src/app/common/index.ts
@@ -0,0 +1,40 @@
+//get createSelector to receive derived parts of state
+import {createSelector} from 'reselect';
+
+import {storeLogger} from "ngrx-store-logger";
+
+//get reducers
+import * as fromLayout from "./layout/layout.reducers";
+
+//import compose to wrap all store actions with logger
+import {compose} from "@ngrx/core";
+import {combineReducers} from "@ngrx/store";
+
+export interface AppState {
+    layout: fromLayout.State
+}
+
+export const reducers = {
+    layout: fromLayout.reducer
+}
+
+/*
+development reducer version, wrapped with storeLogger
+it logs all state changes to console
+ */
+const forDevelopmentReducer: Function = compose (storeLogger(), combineReducers)(reducers);
+
+/*
+"meta" reducer version that transfer current state and dispatched action
+to all the reducers wrapped with logger
+ */
+export function metaReducer(state: any, action: any) {
+    return forDevelopmentReducer(state, action);
+}
+
+/*
+Selectors:
+ */
+export const getLayoutState = (state: AppState) => {
+    state.layout
+}
