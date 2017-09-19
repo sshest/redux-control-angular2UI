501ae0c57969386d9af52c68c1dda9688e4cc11d
sergey.shestakov <sergey.shestakov@uadevelopers.com>
2017-09-19 16:28:14 +0300

    AppComponent transformed into Container component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import {Store} from '@ngrx/store';
+import {Observable} from 'rxjs';
+
+//immport root state for selecting it's parts
+import * as fromRoot from './common/index';
+
+//import layout actions to be able dispatch'em in the component
+import * as layout from './common/layout/layout.actions';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(
+      private store: Store<fromRoot.AppState>
+  ) {
+
+  }
   title = 'app';
 }
